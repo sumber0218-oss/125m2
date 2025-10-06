d01a9c47cae497af8572b4aec519c6611172da90
sumber0218-oss <sumber0218@gmail.com>
2025-10-06 15:44:44 +0800

    125m2

---
+++ data.js
@@ -0,0 +1,431 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "Үүдний хэсэг",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0051458885190439,
+          "pitch": 0.20341796564735048,
+          "rotation": 0,
+          "target": "1---2"
+        },
+        {
+          "yaw": 3.0084922557233504,
+          "pitch": 0.10966337894970835,
+          "rotation": 0,
+          "target": "8-00---2"
+        },
+        {
+          "yaw": 2.4265058090106004,
+          "pitch": 0.11101759773012532,
+          "rotation": 0,
+          "target": "5---2"
+        },
+        {
+          "yaw": 1.3448655403920196,
+          "pitch": 0.20453693572850185,
+          "rotation": 0,
+          "target": "3---1"
+        },
+        {
+          "yaw": 1.174974741570086,
+          "pitch": 0.1987477671171245,
+          "rotation": 0,
+          "target": "6---3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1---2",
+      "name": "Үүдний хэсэг 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.970406213155453,
+          "pitch": 0.14760832067099372,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 1.7293400746438925,
+          "pitch": 0.08515856989602355,
+          "rotation": 0,
+          "target": "6---3"
+        },
+        {
+          "yaw": -2.1794381846820414,
+          "pitch": 0.12224357896501914,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": -2.5044651779388403,
+          "pitch": 0.08143075008554135,
+          "rotation": 0,
+          "target": "8-00---2"
+        },
+        {
+          "yaw": 3.021255818209405,
+          "pitch": 0.12283479601681435,
+          "rotation": 0,
+          "target": "3---1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "Том өрөө",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.936849192710616,
+          "pitch": 0.09494556585598701,
+          "rotation": 0,
+          "target": "1---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---1",
+      "name": "Унтлагын өрөө 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7792610884669777,
+          "pitch": 0.13751259877552258,
+          "rotation": 0,
+          "target": "1---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---",
+      "name": "Хувцас солих өрөө",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.361215833520415,
+          "pitch": 0.050617023845726195,
+          "rotation": 0,
+          "target": "6---3"
+        },
+        {
+          "yaw": -1.6556200958575449,
+          "pitch": 0.1299312873115852,
+          "rotation": 0,
+          "target": "7-00---1"
+        },
+        {
+          "yaw": -2.0006066079320277,
+          "pitch": 0.13576554024672305,
+          "rotation": 0,
+          "target": "1---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5---2",
+      "name": "Унтлагын өрөө 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4714682147671727,
+          "pitch": 0.11419968465133934,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6---3",
+      "name": "Унтлагын өрөө 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9524268046522089,
+          "pitch": 0.0806481625031612,
+          "rotation": 0,
+          "target": "7-00---1"
+        },
+        {
+          "yaw": -0.5156533390433218,
+          "pitch": 0.09945440872600031,
+          "rotation": 0,
+          "target": "4---"
+        },
+        {
+          "yaw": -1.1853583285428186,
+          "pitch": 0.10558082423484016,
+          "rotation": 0,
+          "target": "1---2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-00---1",
+      "name": "00-н өрөө 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.078010646254717,
+          "pitch": 0.20225859518949107,
+          "rotation": 0,
+          "target": "6---3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-00---2",
+      "name": "00-н өрөө 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3142292286542503,
+          "pitch": 0.30921357863404353,
+          "rotation": 0,
+          "target": "1---2"
+        },
+        {
+          "yaw": 0.7145510639776234,
+          "pitch": 0.3366283877151304,
+          "rotation": 0,
+          "target": "5---2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "125m2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
